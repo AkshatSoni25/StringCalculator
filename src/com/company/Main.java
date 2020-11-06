0d3ba607b36c6289aee7346410a8ff4dc909fb12
AkshatSoni25 <akshatsoni2016@gmail.com>
2020-11-06 16:11:07 +0530

    Step 1.

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringCalculator sc = new StringCalculator();
+        System.out.println(sc.Add("23,87,67"));
+    }
+}
+
+class StringCalculator{
+    public int Add(String numbers){
+//        int len = numbers.length();
+        String[] values =numbers.split(",");
+        if(values.length == 0){
+            return 0;
+        } else if(values.length == 1){
+            return Integer.parseInt(values[0]);
+        } else if(values.length == 2){
+            return Integer.parseInt(values[0])+Integer.parseInt(values[1]);
+        }
+        else{
+            return Integer.parseInt(values[0])+Integer.parseInt(values[1])+Integer.parseInt(values[2]);
+        }
+//        for(String s : operators) {
+//            System.out.print(s+ " ");
+//        }
+//        System.out.println();
+//        for(String s : operands) {
+//            System.out.print(s+ " ");
+//        }
+
+    }
+}
